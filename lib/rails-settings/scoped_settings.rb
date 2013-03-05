42cc13b4de3441934fad591f66e30173c396d99d
John Lynch <john@rigelgroupllc.com>
2013-03-04 17:07:05 -0800

    Add ability for object settings to default back to the global setting

--- lib/rails-settings/scoped_settings.rb
+++ lib/rails-settings/scoped_settings.rb
@@ -4,10 +4,10 @@ module RailsSettings
       @object = object
       self
     end
-    
+
     def self.thing_scoped
-      Settings.scoped_by_thing_type_and_thing_id(@object.class.base_class.to_s, @object.id)
+      Settings.where(["(thing_type = ? AND thing_id = ?) OR (thing_type is null AND thing_id is null)", @object.class.base_class.to_s, @object.id]).order("thing_type asc")
     end
- 
+
   end
 end
